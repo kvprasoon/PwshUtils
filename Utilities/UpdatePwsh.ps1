c67f0131820c9c06eada393bc0d8017f691ef263
PRASOON KARUNAN V <12897753+kvprasoon@users.noreply.github.com>
2018-07-19 23:57:12 +0530

    Added error handling and more
    Added below
    - Error handling
    - Opening pwsh post installation

--- Utilities/UpdatePwsh.ps1
+++ Utilities/UpdatePwsh.ps1
@@ -1,3 +1,4 @@
+#requires -RunAsAdministrator
 
 function InstallForWindows{
     Param(
@@ -18,30 +19,39 @@ function InstallForWindows{
     $DownloadPath = "$OutputPath\$($RequiredPackage.Name)"
         
     Write-Host "Downloading latest release $($RequiredPackage.Name) "
-    Invoke-WebRequest $RequiredPackage.Browser_Download_Url -Out "$DownloadPath" 
+    Invoke-WebRequest $RequiredPackage.Browser_Download_Url -Out "$DownloadPath" -ErrorAction Stop
     
     
     Write-Host "Installing $($RequiredPackage.Name)"
-    "msiexec /i $DownloadPath /quiet "| Out-File -FilePath $env:Temp\UpdatePwsh.bat
-    Start-Process -FilePath $env:Temp\UpdatePwsh.bat
+@"
+msiexec /i $DownloadPath /quiet 
+start pwsh
+"@ | Out-File -FilePath $env:Temp\UpdatePwsh.bat
+    Start-Process -FilePath $env:Temp\UpdatePwsh.bat -ErrorAction Stop
 
     Write-Host -ForegroundColor Green "Have fun using Pwsh ..."
     
 
 }
+try{
 
-$Script:pwsh = 'pwsh'
-$ReleaseUrl = "https://api.github.com/repos/PowerShell/PowerShell/releases"
-$CurrentVersion = $PSVersionTable.PSVersion -as [string]
-Write-Host "Installed pwsh version is $CurrentVersion"
-Write-Host "Fetching latest releases"
-$Release = (Invoke-WebRequest $ReleaseUrl | ConvertFrom-Json)[0].assets | Select-Object -Property Name,Browser_Download_Url
+    $Script:pwsh = 'pwsh'
+    $ReleaseUrl = "https://api.github.com/repos/PowerShell/PowerShell/releases"
+    $CurrentVersion = $PSVersionTable.PSVersion -as [string]
+    Write-Host "Installed pwsh version is $CurrentVersion"
+    Write-Host "Fetching latest releases"
+    $Release = (Invoke-WebRequest -Uri $ReleaseUrl | ConvertFrom-Json)[0].assets | Select-Object -Property Name,Browser_Download_Url -ErrorAction Stop
 
-If( $Release.Name -like "*$CurrentVersion*" ){
-    Write-Host "Currenlty executing pwsh is with latest available version"
-    break
+    If( $Release.Name -like "*$CurrentVersion*" ){
+        Write-Host "Currenlty executing pwsh is with latest available version"
+        [System.Environment]::Exit(0)
+    }
+
+    InstallForWindows -Release $Release 
+
+    Stop-Process -Name pwsh -Force -ErrorAction Stop
+
 }
-
-InstallForWindows -Release $Release 
-
-Stop-Process -Name pwsh -Force
+Catch{
+    Throw "pwsh update failed due to $_"
+}
