1c3da030a8dd8a851ce44d030d7b6a47e441e642
PRASOON KARUNAN V <12897753+kvprasoon@users.noreply.github.com>
2019-05-21 22:03:30 +0530

    Added some error handling

--- Utilities/UpdatePwsh.ps1
+++ Utilities/UpdatePwsh.ps1
@@ -65,70 +65,75 @@ Param(
     [Parameter()]
     [switch]$Prerelease
 )
-function InstallForWindows{
-    Param(
-        $Release
-    )
-    $OutputPath = "$env:HOMEPATH\Downloads"
+    function InstallForWindows {
+        Param(
+            $Release
+        )
+        $OutputPath = "$env:HOMEPATH\Downloads"
 
-    if( [Environment]::Is64BitOperatingSystem ){
-        $FilterCriteria  = { ($_.Name -match '.msi') -and ($_.Name -match "x64") }
-    }
-    else{
-        $FilterCriteria  = { ($_.Name -match '.msi') -and ($_.Name -match "x86") }
-    }
+        if ( [Environment]::Is64BitOperatingSystem ) {
+            $FilterCriteria = { ($_.Name -match '.msi') -and ($_.Name -match "x64") }
+        }
+        else {
+            $FilterCriteria = { ($_.Name -match '.msi') -and ($_.Name -match "x86") }
+        }
 
-    Write-Host "Latest released packages are below"
-    $RequiredPackage = $Release | Where-Object $FilterCriteria 
-      
-    $DownloadPath = "$OutputPath\$($RequiredPackage.Name)"
-        
-    Write-Host "Downloading latest release $($RequiredPackage.Name) "
-    Invoke-WebRequest $RequiredPackage.Browser_Download_Url -Out "$DownloadPath" -ErrorAction Stop
-    
-    
-    Write-Host "Installing $($RequiredPackage.Name)"
-@"
-msiexec /i $DownloadPath /quiet 
+        Write-Host "Latest released packages are below"
+        $RequiredPackage = $Release | Where-Object $FilterCriteria
+
+        $DownloadPath = "$OutputPath\$($RequiredPackage.Name)"
+
+        Write-Host "Downloading latest release $($RequiredPackage.Name) "
+        Invoke-WebRequest $RequiredPackage.Browser_Download_Url -Out "$DownloadPath" -ErrorAction Stop
+
+
+        Write-Host "Installing $($RequiredPackage.Name)"
+        @"
+msiexec /i $DownloadPath /quiet
 start pwsh
 "@ | Out-File -FilePath $env:Temp\UpdatePwsh.bat
-    Start-Process -FilePath $env:Temp\UpdatePwsh.bat -ErrorAction Stop
+        Start-Process -FilePath $env:Temp\UpdatePwsh.bat -ErrorAction Stop
 
-    Write-Host -ForegroundColor Green "Have fun using Pwsh ..."
-    
+        Write-Host -ForegroundColor Green "Have fun using Pwsh ..."
 
-}
-try{
 
-    $Script:pwsh    = 'pwsh'
-    $ReleaseUrl     = "https://api.github.com/repos/PowerShell/PowerShell/releases"
-    $MetadataUrl    = "https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/metadata.json"
-    $CurrentVersion = $PSVersionTable.PSVersion -as [string]
-
-    Write-Host "Installed pwsh version is $CurrentVersion"
-    Write-Host "Fetching latest releases"
-
-    $ReleaseMetadata = Invoke-RestMethod -Uri $MetadataUrl -ErrorAction Stop
-    
-    If( $PreRelease.IsPresent ){
-        $ReleseToDownload = $ReleaseMetadata.NextReleaseTag
     }
-    else{
-        $ReleseToDownload = $ReleaseMetadata.ReleaseTag
-    }
+    try {
 
-    $Releases = Invoke-RestMethod -Uri $ReleaseUrl
-    $LatestRelease = ($Releases | Where-Object -FilterScript { $_.Tag_Name -eq $ReleseToDownload }).assets | Select-Object -Property Name,Browser_Download_Url -ErrorAction Stop
+        $Script:pwsh = 'pwsh'
+        $ReleaseUrl = "https://api.github.com/repos/PowerShell/PowerShell/releases"
+        $MetadataUrl = "https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/metadata.json"
+        $CurrentVersion = $PSVersionTable.PSVersion -as [string]
 
-    If( $LatestRelease.Name -like "*$CurrentVersion*" ){
-        Write-Host "Currenlty executing pwsh is with latest available version"        
-    }
-    else{
-        InstallForWindows -Release $LatestRelease 
-        Stop-Process -Name pwsh -Force -ErrorAction Stop
-    }
+        Write-Host "Installed pwsh version is $CurrentVersion"
+        Write-Host "Fetching latest releases"
 
-}
-Catch{
-    Throw "pwsh update failed due to $_"
-}
+        $ReleaseMetadata = Invoke-RestMethod -Uri $MetadataUrl -ErrorAction Stop
+
+        If ( $Preview.IsPresent ) {
+            $ReleseToDownload = $ReleaseMetadata.NextReleaseTag
+        }
+        else {
+            $ReleseToDownload = $ReleaseMetadata.StableReleaseTag
+        }
+
+        $Releases = Invoke-RestMethod -Uri $ReleaseUrl
+        $LatestRelease = ($Releases | Where-Object -FilterScript { $_.Tag_Name -eq $ReleseToDownload }).assets | Select-Object -Property Name, Browser_Download_Url -ErrorAction Stop
+
+        if(($null -eq $LatestRelease)){
+           Write-Warning -Message "$ReleseToDownload is not yet available, try a different version"
+           break
+        }
+
+        If ( $LatestRelease.Name -like "*$CurrentVersion*" ) {
+            Write-Host "Currenlty executing pwsh is with latest available version"
+        }
+        else {
+            InstallForWindows -Release $LatestRelease
+            Stop-Process -Name pwsh -Force -ErrorAction Stop
+        }
+
+    }
+    Catch {
+        Throw "pwsh update failed due to $_"
+    }
